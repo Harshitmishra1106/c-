eead6f9895645fae86e41eddeb6cd3f6a1206036
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-10-07 18:23:02 +0530

    sum_of_two_no's

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter first number\n";
+    cin>>num1;
+    cout<<"enter second number\n";
+    cin>>num2;
+    cout<<"the sum of two no's is :\n"<<num1+num2;
+    return 0;
+}
