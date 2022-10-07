536e4d879c8d22ba55b52e06eb469277b495c810
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-10-07 18:23:49 +0530

    Add files via upload

---
+++ forloop_01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    for ( i = 0; i < n; i++)
+    {
+        cout<<"I will  learn data structure in c++ "<<i+1<<endl;
+    }
+    
+    return 0;
+}
