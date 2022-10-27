ab2ac16736fea406d8dd1fa07ef7056c611e5715
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-10-27 13:48:18 +0530

    Add files via upload

---
+++ FIT.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,x[10],i,X;
+	cin>>T;
+	for(i=0;i<T;i++) cin>>x[i];
+	for(i=0;i<T;i++){ 
+	X=10*x[i];
+	cout<<X<<endl;}
+	return 0;
+}
