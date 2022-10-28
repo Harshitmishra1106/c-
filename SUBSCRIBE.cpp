1325e08a9514e6212fc1cc4521d7f366b34314aa
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-10-28 17:42:16 +0530

    Add files via upload

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,x;
+	cin>>T;
+	while(T--){
+	    cin>>x;
+	    if(x<=30) cout<<"NO\n";
+	    else cout<<"YES\n";
+	}
+	return 0;
+}
