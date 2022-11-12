8faefde069a992226e62f4cd059c296c192f5dcc
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-11-12 21:49:08 +0530

    Add files via upload

--- MAXDIFFMIN.cpp
+++ MAXDIFFMIN.cpp
@@ -2,17 +2,11 @@
 using namespace std;
 
 int main() {
-	int T,A[10],B[10],C[10],i,max,min;
-	cin>>T;
-	for(i=0;i<T;i++) cin>>A[i]>>B[i]>>C[i];
-	for(i=0;i<T;i++){
-	    if(A[i]>B[i]&&A[i]>C[i]) max=A[i];
-	    else if(B[i]>C[i]&&B[i]>A[i]) max=B[i];
-	    else if(C[i]>A[i]&&C[i]>B[i])max=C[i];
-	    if(A[i]<B[i]&&A[i]<C[i]) min=A[i];
-	    else if(B[i]<C[i]&&B[i]<A[i]) min=B[i];
-	    else if(C[i]<A[i]&&C[i]<B[i]) min=C[i];
-	 cout<<max-min<<endl;   
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+	    cin>>a>>b>>c;
+	    cout<<c-a<<endl;
 	}
 	return 0;
 }
