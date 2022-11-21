5a51f0f625cae202d197520ba6a0a42562d7e6ba
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-11-22 00:11:22 +0530

    Add files via upload

---
+++ SPECIALITY.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y,z;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y>>z;
+	    if(x>y&&x>z) cout<<"Setter\n";
+	    else if(y>x&&y>z) cout<<"Tester\n";
+	    else cout<<"Editorialist\n";
+	}
+	return 0;
+}
