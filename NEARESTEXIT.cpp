264d5fa61061cc585574860c89430306302dd25f
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-11-25 19:20:33 +0530

    Add files via upload

---
+++ NEARESTEXIT.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x>50) cout<<"RIGHT\n";
+	    else cout<<"LEFT\n";
+	}
+	return 0;
+}
