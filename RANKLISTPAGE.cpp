0b2b08cff155005b6f6df6ab6f08ed6b70c9393d
Harshitmishra1106 <96685336+Harshitmishra1106@users.noreply.github.com>
2022-11-25 19:19:58 +0530

    Add files via upload

---
+++ RANKLISTPAGE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x%25==0) cout<<x/25<<endl;
+	    else cout<<(x/25)+1<<endl;
+	}
+	return 0;
+}
