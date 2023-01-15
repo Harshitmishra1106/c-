3bb36f2514d8aa3e15624783b6a1bf45b4965f56
Harshit Mishra <96685336+Harshitmishra1106@users.noreply.github.com>
2023-01-15 21:22:33 +0530

    Add files via upload

--- AUDIBLE.cpp
+++ AUDIBLE.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main() {
-	int T,x[10000],i;
-	cin>>T;
-	for(i=0;i<T;i++) cin>>x[i];
-	for(i=0;i<T;i++){
-	    if(x[i]>67&&x[i]<45000) cout<<"YES\n";
-	    else if(x[i]==67||x[i]==45000) cout<<"YES\n";
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x>=67&&x<=45000) cout<<"YES\n";
 	    else cout<<"NO\n";
 	}
 	return 0;
