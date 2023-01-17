92603d019a47b434f039ae37e443121d8280345d
Harshit Mishra <96685336+Harshitmishra1106@users.noreply.github.com>
2023-01-17 20:54:26 +0530

    Add files via upload

---
+++ PAR2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    if(n%2==0) cout<<"Yes\n";
+	    else cout<<"No\n";
+	}
+	return 0;
+}
