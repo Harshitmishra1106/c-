461213be033f13a5063f730f2f63c93403a77792
Harshit Mishra <96685336+Harshitmishra1106@users.noreply.github.com>
2023-01-23 19:35:17 +0530

    Add files via upload

---
+++ LUDO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,die;
+	cin>>t;
+	while(t--){
+	    cin>>die;
+	    if(die==6) cout<<"YES\n";
+	    else cout<<"NO\n";
+	}
+	return 0;
+}
