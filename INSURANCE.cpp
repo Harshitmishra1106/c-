9a71d9d932783c73eff6a45985c3ef3ab15719ad
Harshit Mishra <96685336+Harshitmishra1106@users.noreply.github.com>
2023-02-08 22:32:13 +0530

    Add files via upload

---
+++ INSURANCE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if(y<=x) cout<<y<<endl;
+	    else cout<<x<<endl;
+	}
+	return 0;
+}
